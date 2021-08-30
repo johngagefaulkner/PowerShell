6f80fb2435e2614130fc63c9221d30a5e939d104
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2021-08-30 06:21:10 -0400

    Add files via upload

---
+++ DISM/WindowsHealth-CreateScanReport.ps1
@@ -0,0 +1,20 @@
+﻿<#
+    - Name: WindowsHealth-CreateScanReport
+    - Summary: Runs the DISM "Scan Health" command then generates and returns a report.
+    - Resources and References: https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/repair-a-windows-image
+#>
+Clear-Host
+Write-Host "[WindowsHealth-CreateScanReport]"
+
+# Define Variables
+$reportPath = "C:\Users\Public\WindowsHealth-ScanReport.txt"
+
+# Inform user that the scan is starting
+Write-Host "Initializing Windows Health scan, please wait... "
+Repair-WindowsImage -Online -ScanHealth -NoRestart |Out-File -FilePath $reportPath
+Write-Host "Done! Report saved to: $reportPath"
+
+# Display/return the contents of the results file
+Write-Host " "
+$myResults = Get-Content -Path $reportPath -Raw -Force
+Write-Host $myResults
