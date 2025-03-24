c18f43bc1b79a739e8247c4e9e4e9ee0bca8c7f2
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2025-03-24 15:13:30 -0400

    Update Install-ManageEngineEndpointCentralAgent.ps1

--- Applications/Install-ManageEngineEndpointCentralAgent.ps1
+++ Applications/Install-ManageEngineEndpointCentralAgent.ps1
@@ -5,19 +5,19 @@ Clear-Host
 Write-Host "Installing ManageEngine's EndpointCentral Desktop Agent, please wait... "
 
 ## Define Variables
-$DownloadUrl = "https://desktopcentral.manageengine.com/download?encapiKey=wSsVR61%2F%2FRX4Dap8zzKkdus5zA4HDlOiE05%2F0Fqm7nX4SvuQ8Mc8lE2dAlOiFfgdRWU6F2Ea8bJ4nE0DgWYGi4h8yg0FDCiF9mqRe1U4J3x18O7rwjKeWzs%3D"
+$DownloadUrl = "https://simplifymsp.b-cdn.net/SPPetroluem_Agent.exe"
 $DownloadFolderPath = "C:\"
 $DownloadFileName = "ManageEngineEndpointCentralAgent_Setup.exe"
 $DownloadFullPath = "C:\ManageEngineEndpointCentralAgent_Setup.exe"
 
 if (-not(Test-Path $DownloadFullPath)) {
-	Write-Host "Downloading agent, please wait... "
+    Write-Host "Downloading agent, please wait... "
   	(New-Object System.Net.WebClient).DownloadFile($DownloadUrl, $DownloadFullPath)
-	Write-Host "Done!"
+    Write-Host "Done!"
 }
 
 Set-Location -Path $DownloadFolderPath
-[string]$InstallCmd = "$DownloadFileName -s -r -f1""%systemroot%\temp\install.iss"" /silent"
+[string]$InstallCmd = "$DownloadFileName /silent"
 Write-Host "[Install Command] $InstallCmd"
 Write-Host "Running install command, please wait... "
 
