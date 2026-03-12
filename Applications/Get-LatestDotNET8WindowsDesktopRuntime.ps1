541e76b039f17c659fc20a59d5e25d290b8a03ba
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2026-03-12 14:18:31 -0400

    Update Get-LatestDotNET8WindowsDesktopRuntime.ps1

--- Applications/Get-LatestDotNET8WindowsDesktopRuntime.ps1
+++ Applications/Get-LatestDotNET8WindowsDesktopRuntime.ps1
@@ -1,8 +1,5 @@
 #requires -RunAsAdministrator
-$ProgressPreference = 'SilentlyContinue'
-$ErrorActionPreference = 'SilentlyContinue'
-Clear-Host
 Write-Host "[ Get-LatestDotNET8WindowsDesktopRuntime.ps1 ]" -ForegroundColor Magenta
 Write-Host "Installing latest .NET 8 Windows Desktop Runtime, please wait... "
-winget install Microsoft.DotNet.DesktopRuntime.8 --force --silent
+winget install Microsoft.DotNet.DesktopRuntime.10 --force --silent
 Write-Host "Done!" -ForegroundColor Green
